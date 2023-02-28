109012f552e4fe21ea0e17e813e64bf3c0edf6cd
Ygor Goulart <ygorgm95@gmail.com>
2023-02-28 20:39:06 -0300

    style: add search icon

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -10,32 +10,25 @@ export const Home = () => {
     );
   };
 
-  const SearchFreeCompany = () => {
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchInput(event.target.value);
-      console.log(searchInput);
-    };
-    const handleEnter = () => {};
-    return (
-      <div className="flex flex-col gap-4 justify-center outline outline-1 outline-gray-700 rounded-xl p-8">
-        <h2 className="text-4xl uppercase font-thin">Search Free Company</h2>
-        <input
-          type="text"
-          value={searchInput}
-          onChange={handleChange}
-          placeholder="Name or ID"
-          className="input w-full"
-        />
-      </div>
-    );
-  };
-
   return (
     <div className="bg-neutral-900 text-gray-200 w-full h-screen">
       <div className="flex flex-col gap-4 items-center h-full">
         <Logo />
         <div className="flex flex-col gap-4 h-full justify-center">
-          <SearchFreeCompany />
+          <div className="flex flex-col gap-4 justify-center outline outline-1 outline-gray-700 rounded-xl p-8">
+            <h2 className="text-4xl uppercase font-thin">
+              Search Free Company
+            </h2>
+            <input
+              type="text"
+              name="input"
+              onChange={(e) => setSearchInput(e.target.value)}
+              value={searchInput}
+              placeholder="Name or ID"
+              className="input w-full"
+            />
+            <button></button>
+          </div>
         </div>
       </div>
     </div>
