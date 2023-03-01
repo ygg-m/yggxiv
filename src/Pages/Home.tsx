750da35b41464206f645dd556c462ae98ea18454
Ygor Goulart <ygorgm95@gmail.com>
2023-03-01 01:57:42 -0300

    refactor: input to component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,6 @@
-import { useFreeCompanyContext } from "../Contexts/FreeCompanyContext";
+import { Footer, SearchFreeCompany } from "../Components";
 
 export const Home = () => {
-  const { searchInput, setSearchInput } = useFreeCompanyContext();
   const Logo = () => {
     return (
       <div className="flex text-6xl font-thin text-gray-400 py-4">
@@ -11,24 +10,12 @@ export const Home = () => {
   };
 
   return (
-    <div className="bg-neutral-900 text-gray-200 w-full h-screen">
-      <div className="flex flex-col gap-4 items-center h-full">
+    <div className="bg-neutral-900 text-gray-200 w-full min-h-screen h-full">
+      <div className="flex flex-col gap-8 items-center h-screen">
         <Logo />
-        <div className="flex flex-col gap-4 h-full justify-center">
-          <div className="flex flex-col gap-4 justify-center outline outline-1 outline-gray-700 rounded-xl p-8">
-            <h2 className="text-4xl uppercase font-thin">
-              Search Free Company
-            </h2>
-            <input
-              type="text"
-              name="input"
-              onChange={(e) => setSearchInput(e.target.value)}
-              value={searchInput}
-              placeholder="Name or ID"
-              className="input w-full"
-            />
-            <button></button>
-          </div>
+        <div className="flex flex-col gap-4 h-full justify-between">
+          <SearchFreeCompany />
+          <Footer />
         </div>
       </div>
     </div>
