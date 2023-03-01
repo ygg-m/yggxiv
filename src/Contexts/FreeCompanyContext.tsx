101b95fb27573ee7a174f6438828bb2197dae514
Ygor Goulart <ygorgm95@gmail.com>
2023-03-01 01:58:17 -0300

    feat: fetch free company

--- src/Contexts/FreeCompanyContext.tsx
+++ src/Contexts/FreeCompanyContext.tsx
@@ -1,14 +1,33 @@
 import { createContext, useContext, useState } from "react";
-import { searchFreeCompany } from "../Helpers/xviapi";
+import { FreeCompanySearchResult, getFreeCompanies } from "../Helpers/xviapi";
 
 type FreeCompanyContextType = {
   searchInput: string;
   setSearchInput: any;
+  searchResult: FreeCompanySearchResult;
+  setSearchResult: any;
+  searchFreeCompany: any;
+  fetchLoad: boolean;
 };
 
 const FreeCompanyContext = createContext<FreeCompanyContextType>({
   searchInput: "",
   setSearchInput: () => {},
+  searchResult: {
+    Pagination: {
+      Page: 0,
+      PageNext: 0,
+      PagePrev: 0,
+      PageTotal: 0,
+      Results: 0,
+      ResultsPerPage: 0,
+      ResultsTotal: 0,
+    },
+    Results: [{ Crest: [], ID: "", Name: "", Server: "" }],
+  },
+  setSearchResult: () => {},
+  searchFreeCompany: () => {},
+  fetchLoad: false,
 });
 
 export const useFreeCompanyContext = () => useContext(FreeCompanyContext);
@@ -21,10 +40,36 @@ export const FreeCompanyProvider: React.FC<FreeCompanyProviderProps> = ({
   children,
 }) => {
   const [searchInput, setSearchInput] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<FreeCompanySearchResult>({
+    Pagination: {
+      Page: 0,
+      PageNext: 0,
+      PagePrev: 0,
+      PageTotal: 0,
+      Results: 0,
+      ResultsPerPage: 0,
+      ResultsTotal: 0,
+    },
+    Results: [],
+  });
+  const [fetchLoad, setFetchLoad] = useState<boolean>(false);
+
+  async function searchFreeCompany() {
+    setFetchLoad(true);
+    const search = (await getFreeCompanies(
+      searchInput
+    )) as FreeCompanySearchResult;
+    setSearchResult(search);
+    setFetchLoad(false);
+  }
 
   const value: FreeCompanyContextType = {
     searchInput,
     setSearchInput,
+    searchResult,
+    setSearchResult,
+    searchFreeCompany,
+    fetchLoad,
   };
 
   return (
