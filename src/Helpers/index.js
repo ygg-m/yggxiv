485659097a3366af215c0972b7aed8828f479484
Ygor Goulart <ygorgm95@gmail.com>
2023-03-02 01:33:11 -0300

    create index to Helpers

---
+++ src/Helpers/index.js
@@ -0,0 +1,6 @@
+export { loadFromLocalStorage, saveToLocalStorage } from "./localStorage";
+export { parseDate } from "./parseDate";
+export { parseEstatePlot } from "./parseEstatePlot";
+export { parseStaff } from "./parseStaff";
+export { throttle } from "./throttle";
+export { getFreeCompanies, getFreeCompany } from "./xviapi";
