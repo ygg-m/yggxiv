76011068ea3a38154293226bd51d9f0efb70bff5
Ygor Goulart <ygorgm95@gmail.com>
2023-03-02 16:23:07 -0300

    feat(freeCompany): add basic tab function

--- src/Pages/FreeCompany.tsx
+++ src/Pages/FreeCompany.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { Footer } from "../Components";
 import { useFreeCompanyContext } from "../Contexts/FreeCompanyContext";
 import {
   parseAlliedGC,
@@ -11,7 +13,9 @@ import { FreeCompanyMembersSmall } from "../Helpers/xviapi";
 export const FreeCompany = () => {
   const { freeCompany, fetchFreeCompany, fetchLoad } = useFreeCompanyContext();
   const { FreeCompany } = freeCompany;
-  const { FreeCompanyMembers: Members } = freeCompany;
+  const { ActiveMemberCount } = FreeCompany;
+  const { FreeCompanyMembers } = freeCompany;
+  const [activeTab, setActiveTab] = useState<number>(0);
 
   const { fcId } = useParams();
   if (freeCompany.FreeCompany.ID === "") {
@@ -33,216 +37,272 @@ export const FreeCompany = () => {
     );
   };
 
-  const ProfileImage = () => {
-    const { Crest } = FreeCompany;
-    return (
-      <div className="relative w-32 h-32 overflow-hidden rounded-xl -mt-16 shadow-2xl outline outline-1 outline-neutral-500">
-        <div className="absolute">
-          <img src={Crest[0]} alt="" />
+  const Header = () => {
+    const ProfileImage = () => {
+      const { Crest } = FreeCompany;
+      return (
+        <div className="relative w-32 h-32 overflow-hidden rounded-xl -mt-16 shadow-2xl outline outline-1 outline-neutral-500">
+          <div className="absolute">
+            <img src={Crest[0]} alt="" />
+          </div>
+          <div className="absolute">
+            <img src={Crest[1]} alt="" />
+          </div>
+          <div className="absolute">
+            <img src={Crest[2]} alt="" />
+          </div>
         </div>
-        <div className="absolute">
-          <img src={Crest[1]} alt="" />
-        </div>
-        <div className="absolute">
-          <img src={Crest[2]} alt="" />
-        </div>
-      </div>
-    );
-  };
+      );
+    };
 
-  const FC_Name = () => {
-    const { Name } = FreeCompany;
-    return (
-      <h1 className="text-5xl font-bold flex items-center gap-4">{Name}</h1>
-    );
-  };
+    const FC_Name = () => {
+      const { Name } = FreeCompany;
+      return (
+        <h1 className="text-5xl font-bold flex items-center gap-4">{Name}</h1>
+      );
+    };
 
-  const Server = () => {
-    const { Server, DC } = FreeCompany;
-    const DataCenter = DC.replace("]", "");
-    return (
-      <div className="mt-4 flex gap-2 items-center">
-        <div className="py-3 badge badge-md bg-neutral-800">{DataCenter}</div>
-        <div className="py-3 badge badge-md bg-rose-900 text-rose-200">
-          {Server}
+    const Server = () => {
+      const { Server, DC } = FreeCompany;
+      const DataCenter = DC.replace("]", "");
+      return (
+        <div className="mt-4 flex gap-2 items-center">
+          <div className="py-3 badge badge-md bg-neutral-800">{DataCenter}</div>
+          <div className="py-3 badge badge-md bg-rose-900 text-rose-200">
+            {Server}
+          </div>
         </div>
-      </div>
+      );
+    };
+    return (
+      <header className="w-full flex gap-8 items-center mb-4">
+        <ProfileImage />
+        <FC_Name />
+        <Server />
+      </header>
     );
   };
 
   const Navigator = () => {
-    const { ActiveMemberCount } = FreeCompany;
     return (
       <nav className="flex">
         <div className="tabs">
-          <a className="tab tab-lifted tab-active duration-300">Profile</a>
-          <a className="tab tab-lifted duration-300">
-            Members ({ActiveMemberCount})
-          </a>
+          {tabs.map((tab, index) => (
+            <span
+              key={index}
+              className={`tab tab-lg tab-lifted duration-300 ${
+                index === activeTab ? "tab-active" : ""
+              }`}
+              onClick={() => setActiveTab(index)}
+            >
+              {tab.label}
+            </span>
+          ))}
         </div>
       </nav>
     );
   };
 
-  const Profile = () => {
+  const MainInfo = () => {
     const { Name, Tag, Formed, Rank } = FreeCompany;
     const [FormedDate, FormedTime] = parseDate(Formed);
 
     const BasicInfo = () => {
-      return (
-        <section className="flex flex-col basis-1/2 bg-gray-700 rounded-lg p-6">
-          <div className="flex flex-col">
-            <div className="text-sm opacity-60 italic">Name (Tag)</div>
-            <div>{`${Name} (${Tag})`}</div>
-          </div>
-          <div className="divider"></div>
-
-          <div className="flex flex-col">
-            <div className="text-sm opacity-60 italic">Formed in</div>
-            <div>
-              {FormedDate} - {FormedTime}
+      const Info = () => {
+        return (
+          <section className="flex flex-col basis-1/2 bg-gray-700 rounded-lg p-6">
+            <div className="flex flex-col">
+              <div className="text-sm opacity-60 italic">Name (Tag)</div>
+              <div>{`${Name} (${Tag})`}</div>
             </div>
-          </div>
-          <div className="divider"></div>
+            <div className="divider"></div>
 
-          <div className="flex flex-col">
-            <div className="text-sm opacity-60">Rank</div>
-            <div>{Rank}</div>
+            <div className="flex flex-col">
+              <div className="text-sm opacity-60 italic">Formed in</div>
+              <div>
+                {FormedDate} - {FormedTime}
+              </div>
+            </div>
+            <div className="divider"></div>
+
+            <div className="flex flex-col">
+              <div className="text-sm opacity-60">Rank</div>
+              <div>{Rank}</div>
+            </div>
+          </section>
+        );
+      };
+
+      const Ranks = () => {
+        const { Reputation, Ranking } = FreeCompany;
+        const { Monthly, Weekly } = Ranking;
+        const AlliedGC = parseAlliedGC(Reputation);
+
+        return (
+          <section className="flex flex-col basis-1/2 bg-gray-700 rounded-lg p-6">
+            <div className="flex flex-col">
+              <div className="text-sm opacity-60">Allied Grand Company</div>
+              <div className="flex gap-2">
+                {AlliedGC.Name}
+                {AlliedGC.Icon && (
+                  <img src={AlliedGC.Icon} alt="" className="h-6 rounded-lg" />
+                )}
+              </div>
+            </div>
+            <div className="divider"></div>
+            <div className="flex flex-col">
+              <div className="text-sm opacity-60">Weekly Rank</div>
+              <div>{Weekly}º</div>
+            </div>
+            <div className="divider"></div>
+
+            <div className="flex flex-col">
+              <div className="text-sm opacity-60">Montly Rank</div>
+              <div>{Monthly}º</div>
+            </div>
+          </section>
+        );
+      };
+      return (
+        <article className="grid md:grid-cols-2 p-4 gap-4 bg-base-100 rounded-b-lg rounded-tr-lg">
+          <Info />
+          <Ranks />
+        </article>
+      );
+    };
+
+    const Estate = () => {
+      const {
+        Estate: { Greeting, Name, Plot: rawPlot },
+      } = FreeCompany;
+      const { Size, City, Plot, Ward } = parseEstatePlot(rawPlot);
+
+      return (
+        <section className="grid md:grid-cols-2 bg-base-100 rounded-lg p-4 gap-4">
+          <div className="flex flex-col gap-4 basis-1/2 p-2">
+            <h3 className="text-3xl font-bold">
+              {Name}{" "}
+              <span className="badge badge-md -translate-y-1">{Size}</span>
+            </h3>
+            <h5 className="">{Greeting}</h5>
+          </div>
+
+          <div className="flex bg-gray-700 rounded-lg basis-1/2 justify-between">
+            <div className="flex flex-col items-center justify-center py-4 px-8 basis-full">
+              <div className="text-sm opacity-60 italic">City</div>
+              <div className="text-lg font-bold">{City}</div>
+            </div>
+            <div className="divider divider-horizontal m-0"></div>
+            <div className="flex flex-col items-center justify-center py-4 px-8 basis-full">
+              <div className="text-sm opacity-60 italic">Ward</div>
+              <div className="text-lg font-bold">{Ward}</div>
+            </div>
+            <div className="divider divider-horizontal m-0"></div>
+
+            <div className="flex flex-col items-center justify-center p-4 basis-full">
+              <div className="text-sm opacity-60 italic">Plot</div>
+              <div className="text-lg font-bold">{Plot}</div>
+            </div>
           </div>
         </section>
       );
     };
 
-    const Ranks = () => {
-      const { Reputation, Ranking } = FreeCompany;
-      const { Monthly, Weekly } = Ranking;
-      const AlliedGC = parseAlliedGC(Reputation);
+    const Staff = () => {
+      const Staffs = parseStaff(
+        FreeCompanyMembers
+      ) as FreeCompanyMembersSmall[];
+
+      type CardProps = {
+        Avatar: string;
+        ID: number;
+        Name: string;
+        Rank: string;
+        RankIcon: string;
+      };
+
+      const Card: React.FC<CardProps> = ({
+        Avatar,
+        ID,
+        Name,
+        Rank,
+        RankIcon,
+      }) => {
+        return (
+          <Link to={`/Character/${ID}`}>
+            <article className="flex gap-4 p-6 outline outline-1 outline-gray-700 rounded-lg hover:bg-rose-900 hover:outline-rose-900 hover:text-rose-200 duration-300 cursor-pointer">
+              <img
+                src={Avatar}
+                alt={`Profile Picture from ${Name}`}
+                className="h-16 w-16 rounded-full"
+              />
+              <div className="flex flex-col gap-2 justify-center">
+                <span className="font-bold">{Name}</span>
+                <span className="flex gap-2 items-center">
+                  <img src={RankIcon} alt={"Rank Icon"} />
+                  {Rank}
+                </span>
+              </div>
+            </article>
+          </Link>
+        );
+      };
 
       return (
-        <section className="flex flex-col basis-1/2 bg-gray-700 rounded-lg p-6">
-          <div className="flex flex-col">
-            <div className="text-sm opacity-60">Allied Grand Company</div>
-            <div className="flex gap-2">
-              {AlliedGC.Name}
-              {AlliedGC.Icon && (
-                <img src={AlliedGC.Icon} alt="" className="h-6 rounded-lg" />
-              )}
-            </div>
-          </div>
-          <div className="divider"></div>
-          <div className="flex flex-col">
-            <div className="text-sm opacity-60">Weekly Rank</div>
-            <div>{Weekly}º</div>
-          </div>
-          <div className="divider"></div>
-
-          <div className="flex flex-col">
-            <div className="text-sm opacity-60">Montly Rank</div>
-            <div>{Monthly}º</div>
-          </div>
+        <section className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {Staffs.map((member) => {
+            const { Avatar, ID, Name, Rank, RankIcon } = member;
+            return (
+              <Card
+                Avatar={Avatar}
+                ID={ID}
+                Name={Name}
+                Rank={Rank}
+                RankIcon={RankIcon}
+              />
+            );
+          })}
         </section>
       );
     };
 
     return (
-      <article className="flex p-4 gap-4 bg-gray-800 rounded-b-lg rounded-tr-lg">
+      <section className="grid gap-4 pb-8">
         <BasicInfo />
-        <Ranks />
-      </article>
-    );
-  };
-
-  const Estate = () => {
-    const {
-      Estate: { Greeting, Name, Plot: rawPlot },
-    } = FreeCompany;
-    const { Size, City, Plot, Ward } = parseEstatePlot(rawPlot);
-
-    return (
-      <section className="flex basis-1/2 bg-gray-800 rounded-lg p-4 mt-4 gap-4">
-        <div className="flex flex-col gap-4 basis-1/2 p-2">
-          <h3 className="text-3xl font-bold">
-            {Name} <span className="badge badge-md -translate-y-1">{Size}</span>
-          </h3>
-          <h5 className="">{Greeting}</h5>
-        </div>
-
-        <div className="flex bg-gray-700 rounded-lg basis-1/2 justify-between">
-          <div className="flex flex-col items-center justify-center py-4 px-8 basis-full">
-            <div className="text-sm opacity-60 italic">City</div>
-            <div className="text-lg font-bold">{City}</div>
-          </div>
-          <div className="divider divider-horizontal m-0"></div>
-          <div className="flex flex-col items-center justify-center py-4 px-8 basis-full">
-            <div className="text-sm opacity-60 italic">Ward</div>
-            <div className="text-lg font-bold">{Ward}</div>
-          </div>
-          <div className="divider divider-horizontal m-0"></div>
-
-          <div className="flex flex-col items-center justify-center p-4 basis-full">
-            <div className="text-sm opacity-60 italic">Plot</div>
-            <div className="text-lg font-bold">{Plot}</div>
-          </div>
-        </div>
+        <Estate />
+        <Staff />
       </section>
     );
   };
 
-  const Staff = () => {
-    const Staffs = parseStaff(Members) as FreeCompanyMembersSmall[];
-
-    type CardProps = {
-      Avatar: string;
-      ID: number;
-      Name: string;
-      Rank: string;
-      RankIcon: string;
-    };
-
-    const Card: React.FC<CardProps> = ({
-      Avatar,
-      ID,
-      Name,
-      Rank,
-      RankIcon,
-    }) => {
-      return (
-        <Link to={`/Character/${ID}`}>
-          <article className="flex gap-4 p-6 outline outline-1 outline-gray-700 rounded-lg hover:bg-rose-900 hover:outline-rose-900 hover:text-rose-200 duration-300 cursor-pointer">
-            <img
-              src={Avatar}
-              alt={`Profile Picture from ${Name}`}
-              className="h-16 w-16 rounded-full"
-            />
-            <div className="flex flex-col gap-2 justify-center">
-              <span className="font-bold">{Name}</span>
-              <span className="flex gap-2 items-center">
-                <img src={RankIcon} alt={"Rank Icon"} />
-                {Rank}
-              </span>
-            </div>
-          </article>
-        </Link>
-      );
-    };
-
+  const Members = () => {
     return (
-      <section className="mt-4 pb-8 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {Staffs.map((member) => {
-          const { Avatar, ID, Name, Rank, RankIcon } = member;
-          return (
-            <Card
-              Avatar={Avatar}
-              ID={ID}
-              Name={Name}
-              Rank={Rank}
-              RankIcon={RankIcon}
-            />
-          );
-        })}
-      </section>
+      <section className="grid gap-4 pb-8">Content for Members Tab</section>
     );
   };
+
+  const Ranks = () => {
+    return <section className="grid gap-4 pb-8">Content for Ranks Tab</section>;
+  };
+
+  const Stats = () => {
+    return <section className="grid gap-4 pb-8">Content for Stats Tab</section>;
+  };
+
+  const tabs = [
+    {
+      label: "Info",
+      content: <MainInfo />,
+    },
+    {
+      label: `Members (${ActiveMemberCount})`,
+      content: <Members />,
+    },
+    {
+      label: "Ranks",
+      content: <Ranks />,
+    },
+  ];
 
   if (fetchLoad)
     return (
@@ -255,16 +315,11 @@ export const FreeCompany = () => {
     <div className="w-screen bg-neutral-900 min-h-screen flex flex-col items-center">
       <CoverImage />
       <div className="w-screen max-w-screen-2xl flex flex-col px-8">
-        <header className="w-full flex gap-8 items-center mb-4">
-          <ProfileImage />
-          <FC_Name />
-          <Server />
-        </header>
+        <Header />
         <Navigator />
-        <Profile />
-        <Estate />
-        <Staff />
+        <div className="tab-content">{tabs[activeTab].content}</div>
       </div>
+      <Footer />
     </div>
   );
 };
