dd10cd7abac5c7a6885d71ac372df8bf6aa8e2cc
Ygor Goulart <ygorgm95@gmail.com>
2023-03-02 01:51:45 -0300

    fix: export FreeCompanyMembersSmall types

--- src/Helpers/xviapi.ts
+++ src/Helpers/xviapi.ts
@@ -101,7 +101,7 @@ interface FreeCompanyData {
   Tag: string;
 }
 
-interface FreeCompanyMembersSmall {
+export interface FreeCompanyMembersSmall {
   Avatar: string;
   FeastMatches: number;
   ID: number;
