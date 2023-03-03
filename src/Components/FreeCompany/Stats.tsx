51b88094e0bc2eee7f2db82fb688fc7c5f0af2b1
Ygor Goulart <ygorgm95@gmail.com>
2023-03-03 15:55:55 -0300

    base component for Ranks/Stats

--- src/Components/FreeCompany/Stats.tsx
+++ src/Components/FreeCompany/Stats.tsx
@@ -1,7 +1,14 @@
 export const Stats = () => {
   return (
     <section className="grid gap-4 pb-8 min-h-[calc(100vh-448px)]">
-      Content for Stats Tab
+      <nav className="navbar grid bg-base-100 rounded-lg h-fit">
+        <button className="btn hover:bg-primary gap-2 flex-col py-4 h-fit">
+          Pull Character data{" "}
+          <span className="lowercase font-normal opacity-60">
+            (may take a while)
+          </span>
+        </button>
+      </nav>
     </section>
   );
 };
