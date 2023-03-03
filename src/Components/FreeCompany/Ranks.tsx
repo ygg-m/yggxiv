91a6cce0576c5c84b64246fb7be012daf75c4b93
Ygor Goulart <ygorgm95@gmail.com>
2023-03-03 14:02:07 -0300

    feat: add Ranks&Stats tabs to FreeCompany page

---
+++ src/Components/FreeCompany/Ranks.tsx
@@ -0,0 +1,7 @@
+export const Ranks = () => {
+  return (
+    <section className="grid gap-4 pb-8 min-h-[calc(100vh-448px)]">
+      Content for Ranks Tab
+    </section>
+  );
+};
---
+++ src/Components/FreeCompany/Stats.tsx
@@ -0,0 +1,3 @@
+export const Stats = () => {
+  return <section className="grid gap-4 pb-8">Content for Stats Tab</section>;
+};
