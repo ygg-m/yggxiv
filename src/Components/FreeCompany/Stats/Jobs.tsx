c54fd19e2feea1d33389b54b69fef6bcf90eae8b
Ygor Goulart <ygorgm95@gmail.com>
2023-03-06 20:51:21 -0300

    chores: remove unused imports

--- src/Components/FreeCompany/Stats/Jobs.tsx
+++ src/Components/FreeCompany/Stats/Jobs.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useFreeCompanyContext } from "../../../Contexts/FreeCompanyContext";
 import { jobs } from "../../../Data/jobs";
