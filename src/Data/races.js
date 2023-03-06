4553b327671437193f417ff362b69ce9990f2b6d
Ygor Goulart <ygorgm95@gmail.com>
2023-03-06 20:50:59 -0300

    create base data for races

---
+++ src/Data/races.js
@@ -0,0 +1,50 @@
+export const races = [
+  {
+    ID: 1,
+    Icon: null,
+    Name: "Hyur",
+    Clan: ["Midlander", "Highlander"],
+  },
+  {
+    ID: 2,
+    Icon: null,
+    Name: "Elezen",
+    Clan: ["Wildwood", "Duskwight"],
+  },
+  {
+    ID: 3,
+    Icon: null,
+    Name: "Lalafell",
+    Clan: ["Plainsfolk", "Dunesfolk"],
+  },
+  {
+    ID: 4,
+    Icon: null,
+    Name: "Miqo'te",
+    Clan: ["Seeker of the Sun", "Keeper of the Moon"],
+  },
+  {
+    ID: 5,
+    Icon: null,
+    Name: "Roegadyn",
+    Clan: ["Sea Wolves", "Hellsguard"],
+  },
+  {
+    ID: 6,
+    Icon: null,
+    Name: "Au Ra",
+    Clan: ["Raen", "Xaela"],
+  },
+  {
+    ID: 7,
+    Icon: null,
+    Name: "Hrothgar",
+    Clan: ["Helions", "The Lost"],
+  },
+  {
+    ID: 8,
+    Icon: null,
+    Name: "Viera",
+    Clan: ["Rava", "Veena"],
+  },
+];
