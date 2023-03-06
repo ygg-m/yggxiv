7cd5dc14061cce249f78fd740ff1335d06e1841f
Ygor Goulart <ygorgm95@gmail.com>
2023-03-06 20:51:11 -0300

    chores: base imports for races

--- src/Components/FreeCompany/Stats/Races.tsx
+++ src/Components/FreeCompany/Stats/Races.tsx
@@ -1,3 +1,10 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useFreeCompanyContext } from "../../../Contexts/FreeCompanyContext";
+import { races } from "../../../Data/races";
+
 export const Races = () => {
-  return <div>Races</div>;
+  return <div>
+    
+  </div>;
 };
