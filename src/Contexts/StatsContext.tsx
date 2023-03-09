61039a4d3728615daf033f070f243458a36196ed
Ygor Goulart <ygorgm95@gmail.com>
2023-03-09 16:22:40 -0300

    chores: create StatsContext base states

--- src/Contexts/StatsContext.tsx
+++ src/Contexts/StatsContext.tsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { getFreeCompanies } from "../Helpers";
-import { FreeCompanySearchResult } from "../Types";
+import { useFreeCompany } from "./FreeCompanyContext";
 
 type FCStatsContextType = {};
 
@@ -13,6 +12,31 @@ type CharacterContextProps = { children: React.ReactNode };
 export const FCStatsProvider: React.FC<CharacterContextProps> = ({
   children,
 }) => {
+  const { MembersFullData } = useFreeCompany();
+
+  // Leaderboards
+  const [leaderboardMount, setLeaderboardMount] = useState();
+  const [leaderboardMinion, setLeaderboardMinion] = useState();
+  const [leaderboardAchievement, setLeaderboardAchievement] = useState();
+
+  // Character
+  const [popularRaces, setPopularRaces] = useState();
+  const [popularGender, setPopularGender] = useState();
+
+  // Jobs
+  const [popularJobs, setPopularJobs] = useState();
+
+  // Mount
+  const [popularMount, setPopularMount] = useState();
+  const [rarestMount, setRarestMount] = useState();
+
+  // Minion
+  const [popularMinion, setPopularMinion] = useState();
+  const [rarestMinion, setRarestMinion] = useState();
+
+  // Achievement
+  const [rarestAchievement, setRarestAchievement] = useState();
+
   const value: FCStatsContextType = {};
 
   return (
