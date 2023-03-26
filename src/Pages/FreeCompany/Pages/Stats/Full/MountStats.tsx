7e8284de71f068409f1dedc9ff5b44b01db87d24
Ygor Goulart <ygorgm95@gmail.com>
2023-03-25 22:38:10 -0300

    style: change "card" to "table"-like desing

--- src/Pages/FreeCompany/Pages/Stats/Full/MountStats.tsx
+++ src/Pages/FreeCompany/Pages/Stats/Full/MountStats.tsx
@@ -1,9 +1,10 @@
+import { PlusIcon } from "@/Assets/Images/UI";
 import { useFreeCompany } from "@/Contexts/FreeCompanyContext";
 import { useStats } from "@/Contexts/StatsContext";
 import { reverseArray } from "@/Helpers/reverseArray";
 import { CollectibleData, CollectibleTypes } from "@/Types";
 import "chart.js/auto";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Chart } from "react-chartjs-2";
 import { Link, useLocation } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
@@ -106,11 +107,12 @@ export const MountStats = () => {
     );
   };
 
-  interface CollectibleCardProps {
+  interface CollectibleItemListProps {
     data: CollectibleTypes;
+    index: number;
   }
 
-  const CollectibleCard = ({ data }: CollectibleCardProps) => {
+  const CollectibleItemList = ({ data, index }: CollectibleItemListProps) => {
     const {
       freeCompany: {
         FreeCompany: { ActiveMemberCount },
@@ -122,60 +124,188 @@ export const MountStats = () => {
 
     const percentage = (Count / ActiveMemberCount) * 100;
 
-    return (
-      <div className="grid rounded-lg bg-base-100 p-4">
-        <div className="flex items-center gap-4">
-          <img
-            src={`http://xivapi.com${Icon}`}
-            alt={Name}
-            className="mask mask-squircle"
+    const Placement = () => (
+      <div className="col-span-1 grid place-items-center">{index + 1}</div>
+    );
+
+    const CollectableIcon = () => (
+      <div className="col-span-1">
+        <img
+          src={`http://xivapi.com${Icon}`}
+          alt={Name}
+          className="mask mask-squircle w-12"
+        />
+      </div>
+    );
+
+    const Counter = () => (
+      <div
+        className="tooltip col-span-1 grid cursor-default place-items-center rounded-lg text-primary duration-300 hover:bg-base-300"
+        data-tip={`${
+          percentage > 1 ? Math.floor(percentage) : percentage.toFixed(1)
+        }%`}
+      >
+        <div>{Count}</div>
+      </div>
+    );
+
+    const CollectableName = () => (
+      <div className="col-span-1 grid items-center capitalize">{Name}</div>
+    );
+
+    const OwnerList = () => {
+      const [indexes, setIndexes] = useState(0);
+      const list = useMemo(() => Owners.slice(0, indexes), [Owners, indexes]);
+      const divRef = useRef<HTMLDivElement>(null);
+
+      useEffect(() => {
+        if (divRef.current) {
+          const newSize = Math.floor(divRef.current.offsetWidth / 40);
+          setIndexes(newSize);
+        }
+
+        const handleResize = () => {
+          if (divRef.current) {
+            const newSize = Math.floor(divRef.current.offsetWidth / 40);
+
+            setIndexes(newSize);
+          }
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        // Cleanup function to remove event listener
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, [divRef]);
+
+      const OwnerItem = ({ data }: any) => {
+        const { Avatar, Name, ID } = data.Character;
+        return (
+          <Link
+            to={`Character/ID`}
+            className="tooltip z-10 rounded-lg p-1 duration-300 hover:bg-neutral"
+            data-tip={Name}
+          >
+            <img src={Avatar} alt={Name} className="mask mask-squircle w-8" />
+          </Link>
+        );
+      };
+
+      const OwnerCard = ({ data }: any) => {
+        const { Avatar, Name, ID } = data.Character;
+
+        const SplitName = Name.split(" ");
+
+        return (
+          <Link
+            to={`Character/ID`}
+            className="grid place-items-center rounded-lg p-2 text-center duration-300 hover:bg-neutral"
+          >
+            <img src={Avatar} alt={Name} className="mask mask-squircle w-12" />
+            <span>{SplitName[0]}</span>
+            <span>{SplitName[1]}</span>
+          </Link>
+        );
+      };
+
+      const Modal = () => (
+        <>
+          <label htmlFor={`MountModal-${index}`} className="btn-secondary btn">
+            See all
+          </label>
+
+          <input
+            type="checkbox"
+            id={`MountModal-${index}`}
+            className="modal-toggle"
           />
 
-          <h3 className="w-[12rem] text-lg capitalize">{Name}</h3>
-
-          <div className="flex flex-wrap rounded-lg bg-neutral p-2">
-            {Owners.map((owner) => {
-              const { Avatar, Name, ID } = owner.Character;
-              return (
-                <div
-                  className="tooltip rounded-lg bg-transparent p-2 duration-300 hover:bg-base-200"
-                  data-tip={Name}
-                  key={uuidv4()}
-                >
-                  <img
-                    src={Avatar}
-                    alt={Name}
-                    className="mask mask-squircle w-12"
-                  />
-                </div>
-              );
-            })}
+          <div className="modal">
+            <div className="modal-box">
+              <div className="grid grid-cols-4 items-center gap-2">
+                {Owners.map((Owner) => (
+                  <OwnerCard key={uuidv4()} data={Owner} />
+                ))}
+              </div>
+            </div>
+            <label
+              htmlFor={`MountModal-${index}`}
+              className="btn-sm btn-circle btn fixed right-2 top-2"
+            >
+              ✕
+            </label>
           </div>
+        </>
+      );
 
-          <div className="grid place-items-center">
-            <h2 className="flex items-center gap-2 text-3xl font-bold text-primary">
-              {Count}
-            </h2>
-            <span className="text-lg font-normal text-neutral-content">
-              {Count === 1 ? "Owner" : "Owners"}
-            </span>
-            <h3 className="opacity-80">
-              (
-              {percentage >= 1 ? Math.floor(percentage) : percentage.toFixed(1)}
-              %)
-            </h3>
+      return (
+        <div
+          className="col-span-1 flex items-center justify-between"
+          ref={divRef}
+        >
+          <div className="flex items-center">
+            {list.map((Owner) => (
+              <OwnerItem key={uuidv4()} data={Owner} />
+            ))}
           </div>
+          {Owners.length > indexes ? <Modal /> : null}
         </div>
+      );
+    };
+
+    return (
+      <div className="grid grid-cols-[2rem_3rem_4rem_.25fr_1fr] grid-rows-1 gap-2 border-b border-neutral-700 bg-neutral py-1 duration-300 hover:bg-base-100">
+        <Placement />
+        <CollectableIcon />
+        <Counter />
+        <CollectableName />
+        <OwnerList />
       </div>
     );
   };
 
-  const CollectibleCards = () => {
+  const CollectibleList = () => {
+    const [indexes, setIndexes] = useState(10);
+    const list = useMemo(() => filter.slice(0, indexes), [filter, indexes]);
+
+    useEffect(() => {
+      setIndexes(10);
+    }, [filter]);
+
     return (
-      <div className="mt-4 grid gap-4">
-        {filter.map((Collectible) => (
-          <CollectibleCard key={uuidv4()} data={Collectible} />
-        ))}
+      <div className="mt-2 grid gap-4 rounded-t-lg">
+        <div className="rounded-lg outline outline-1 outline-neutral-700">
+          <div className="grid grid-cols-[2rem_3rem_4rem_.25fr_1fr] grid-rows-1 gap-2 border-b border-neutral-700 py-2">
+            <div className="col-span-1 grid place-items-center opacity-70">
+              #
+            </div>
+            <div className="col-span-1 text-sm opacity-70"></div>
+            <div className="col-span-1 text-sm opacity-70">Count</div>
+            <div className="col-span-1 text-sm opacity-70">Name</div>
+            <div className="col-span-1 text-sm opacity-70">Owners</div>
+          </div>
+
+          <div className="grid rounded-b-lg">
+            {list.map((Collectible, index) => (
+              <CollectibleItemList
+                key={uuidv4()}
+                data={Collectible}
+                index={index}
+              />
+            ))}
+          </div>
+        </div>
+
+        {filter.length > indexes ? (
+          <button
+            className="btn-secondary btn w-full"
+            onClick={() => setIndexes(indexes + 10)}
+          >
+            Show More
+          </button>
+        ) : null}
       </div>
     );
   };
@@ -193,7 +323,7 @@ export const MountStats = () => {
       Click: (index: number) => {
         setTabIndex(index);
       },
-      Content: <CollectibleCards />,
+      Content: <CollectibleList />,
     },
   ];
 
@@ -274,12 +404,14 @@ export const MountStats = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="mt-4 flex w-full justify-between">
+      <div className="mt-4 flex w-full flex-col justify-between gap-2 sm:flex-row">
         <div className="flex gap-2">
-          <div className="tabs tabs-boxed">
+          <div className="tabs tabs-boxed w-full sm:w-fit">
             {Tabs.map((tab, index) => (
               <button
-                className={`tab ${index === tabIndex ? "tab-active" : ""}`}
+                className={`tab w-1/2 sm:w-fit ${
+                  index === tabIndex ? "tab-active" : ""
+                }`}
                 onClick={() => tab.Click(index)}
                 key={uuidv4()}
               >
@@ -288,35 +420,53 @@ export const MountStats = () => {
             ))}
           </div>
 
-          <div className="tabs tabs-boxed">
+          <div className="tabs tabs-boxed w-full sm:w-fit">
             <Link
               to="Popular"
-              className={`tab ${!filterRarest ? "tab-active" : ""}`}
+              className={`tab w-1/2 sm:w-fit ${
+                !filterRarest ? "tab-active" : ""
+              }`}
             >
               Popular
             </Link>
             <Link
               to="Rarest"
-              className={`tab ${filterRarest ? "tab-active" : ""}`}
+              className={`tab w-1/2 sm:w-fit ${
+                filterRarest ? "tab-active" : ""
+              }`}
             >
               Rarest
             </Link>
           </div>
         </div>
-        <div className="form-control">
-          <input
-            value={query}
-            type="text"
-            placeholder="Search by Name"
-            onChange={(e) => setQuery(e.target.value)}
-            className="input-bordered input h-[40px]"
-          />
+        <div className="flex gap-2">
+          <div className="form-control -order-1 sm:order-1">
+            <input
+              value={query}
+              type="text"
+              placeholder="Search Character"
+              onChange={(e) => setQuery(e.target.value)}
+              className="input-bordered input h-[40px]"
+            />
+          </div>
+
+          <div className="form-control -order-1 sm:order-1">
+            <input
+              value={query}
+              type="text"
+              placeholder="Seach Mount"
+              onChange={(e) => setQuery(e.target.value)}
+              className="input-bordered input h-[40px]"
+            />
+          </div>
         </div>
       </div>
-      <div className="tabs tabs-boxed mt-2 flex w-fit items-center px-2">
+      <div className="tabs tabs-boxed mt-2 flex items-center px-2 sm:w-fit">
         {SourceTabs.map((tab, index) => (
           <button
-            className={`tab ${index === sourceTabIndex ? "tab-active" : ""}`}
+            className={`tab w-1/5 ${
+              index === sourceTabIndex ? "tab-active" : ""
+            }`}
             onClick={() => tab.Click(index)}
             key={uuidv4()}
           >
