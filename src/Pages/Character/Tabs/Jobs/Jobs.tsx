0c3ede6c7bc75b1c2ac430c369e19c6985317eb5
Ygor Goulart <ygorgm95@gmail.com>
2023-04-02 14:32:52 -0300

    style: bozjan/eureka better placement

--- src/Pages/Character/Tabs/Jobs/Jobs.tsx
+++ src/Pages/Character/Tabs/Jobs/Jobs.tsx
@@ -17,7 +17,7 @@ export const CharJobs = () => {
 
   const SpecialJobs = () => {
     return BozData || ElData ? (
-      <section className="flex gap-2">
+      <section className="flex gap-4 rounded-lg bg-base-100 p-4">
         <Bozjan data={BozData} />
         <Elemental data={ElData} />
       </section>
@@ -26,27 +26,27 @@ export const CharJobs = () => {
 
   const BattleJobs = () => {
     return (
-      <>
+      <article className="grid md:grid-cols-2">
         <Tanks data={Battle.Tanks} />
         <Healers data={Battle.Healers} />
         <Melee data={Battle.Melee} />
         <RangedPhys data={Battle.RangedPhys} />
         <RangedMagic data={Battle.RangedMagic} />
-      </>
+      </article>
     );
   };
 
   const CraftGatherJobs = () => {
     return (
-      <>
+      <article>
         <Crafters data={CraftGather.Craft} />
         <Gatherers data={CraftGather.Gather} />
-      </>
+      </article>
     );
   };
 
   return (
-    <article className="flex flex-col gap-4 rounded-lg rounded-b-lg bg-base-100 p-4">
+    <article className="flex flex-col gap-4">
       <SpecialJobs />
       <BattleJobs />
       <CraftGatherJobs />
