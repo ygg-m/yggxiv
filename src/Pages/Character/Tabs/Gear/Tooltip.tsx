c38ec81f95a79430d577e3137e1756d63b85e656
Ygor Goulart <ygorgm95@gmail.com>
2023-04-02 20:29:31 -0300

    refactor: tooltips

--- src/Pages/Character/Tabs/Gear/Tooltip.tsx
+++ src/Pages/Character/Tabs/Gear/Tooltip.tsx
@@ -1,4 +1,5 @@
 import { ItemData, ItemFetchData } from "@/Types/CharacterData";
+import { useState } from "react";
 
 const CreateMateriaSlots = (slots: number, equipped: ItemFetchData[]) => {
   const elements = [];
@@ -19,68 +20,107 @@ const CreateMateriaSlots = (slots: number, equipped: ItemFetchData[]) => {
 
 const MateriaSlot = ({ data }: { data: ItemFetchData }) => {
   const { Icon, Name } = data;
+  const [showTooltip, setShowTooltip] = useState(false);
 
   return (
-    <div className="flex items-center gap-2 rounded-lg hover:bg-base-300">
+    <div
+      className="relative flex items-center gap-2 rounded-lg hover:bg-base-300"
+      onMouseOver={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+      onClick={() => {
+        setShowTooltip(true);
+      }}
+    >
       <img src={Icon} className="h-6 w-6 rounded-full bg-base-100" />
+      {Name}
+      {showTooltip ? <ItemTooltip data={data} /> : null}
+    </div>
+  );
+};
+
+const Materias = ({
+  slots,
+  equipped,
+}: {
+  slots: number;
+  equipped: ItemFetchData[];
+}) => {
+  return (
+    <div className="grid gap-2">
+      <span className="relative opacity-70">Materia</span>
+      <div className="grid gap-1">
+        {equipped.map((materia) => (
+          <MateriaSlot data={materia} />
+        ))}
+        {CreateMateriaSlots(slots, equipped)}
+      </div>
+    </div>
+  );
+};
+
+const ItemsLevels = ({
+  equipLv,
+  itemLv,
+}: {
+  equipLv: number;
+  itemLv: number;
+}) => {
+  return (
+    <div className="flex justify-between gap-2">
+      <span className="flex gap-2">
+        <span className="opacity-70">Equip Level</span>
+        {equipLv}
+      </span>
+
+      <span className="flex gap-2">
+        <span className="opacity-70">Item Level</span>
+        {itemLv}
+      </span>
+    </div>
+  );
+};
+
+const ItemTooltip = ({ data }: { data: ItemFetchData }) => {
+  const { Name } = data;
+  return (
+    <div className="absolute bottom-[50%] left-[50%] z-10 grid w-60 gap-2 rounded-lg bg-base-300 p-3 text-sm shadow-md outline outline-1 outline-slate-700">
       {Name}
     </div>
   );
 };
 
+const ShowGlamour = ({ data }: { data: ItemFetchData }) => {
+  const { Icon, Name } = data;
+  return (
+    <div className="grid gap-2">
+      <span className="relative opacity-70">Glamour</span>
+      <div className="relative flex items-center gap-2 rounded-lg hover:bg-base-300">
+        <img src={Icon} className="h-6 w-6 rounded-full bg-base-100" />
+        <span className="text-xs">{Name}</span>
+      </div>
+    </div>
+  );
+};
+
 export const GearTooltip = ({ data }: { data: ItemData }) => {
-  const {
-    ID,
-    Name,
-    ItemLevel,
-    EquipLevel,
-    MateriaSlots,
-    MateriaEquipped,
-    Glamour,
-  } = data;
+  const { ID, Name, ItemLevel, EquipLevel, MateriaSlots, MateriaEquipped } =
+    data;
 
   return (
-    <div className="absolute bottom-[50%] left-[50%] z-10 grid w-60 gap-2 rounded-lg bg-neutral p-3 text-sm shadow-md">
+    <div className="absolute bottom-[50%] left-[50%] z-10 grid w-60 gap-2 rounded-lg bg-neutral p-3 text-sm shadow-md outline outline-1 outline-slate-700">
       <span>{Name}</span>
 
       <div className="divider m-0 h-fit"></div>
 
-      <div className="flex justify-between gap-2">
-        <span className="flex gap-2">
-          <span className="opacity-70">Equip Level</span>
-          {EquipLevel}
-        </span>
-
-        <span className="flex gap-2">
-          <span className="opacity-70">Item Level</span>
-          {ItemLevel}
-        </span>
-      </div>
+      <ItemsLevels equipLv={EquipLevel} itemLv={ItemLevel} />
 
       <div className="divider m-0 h-fit"></div>
 
-      <div className="grid gap-2">
-        <span className="relative opacity-70">Materia</span>
-        <div className="grid gap-1">
-          {MateriaEquipped.map((materia) => (
-            <MateriaSlot data={materia} />
-          ))}
-          {CreateMateriaSlots(MateriaSlots, MateriaEquipped)}
-        </div>
-      </div>
+      <Materias slots={MateriaSlots} equipped={MateriaEquipped} />
 
       <div className="divider m-0 h-fit"></div>
 
-      <div className="grid gap-2">
-        <span className="relative opacity-70">Glamour</span>
-        <div className="relative flex items-center gap-2 rounded-lg hover:bg-base-300">
-          <img
-            src={Glamour.Icon}
-            className="h-6 w-6 rounded-full bg-base-100"
-          />
-          <span className="text-xs">{Glamour.Name}</span>
-        </div>
-      </div>
+      <ShowGlamour data={data.Glamour} />
 
       <div className="divider m-0 h-fit"></div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      gold: "#FFD700",
-      silver: "#COCOCO",
-      bronze: "#CD7F32",
-      tank: "#3e51b1",
-      healer: "#3c672f",
-      dps: "#743333",
-      gatherer: "#a28838",
-      crafter: "#6748ad",
+    extend: {
+      colors: {
+        gold: "#FFD700",
+        silver: "#COCOCO",
+        bronze: "#CD7F32",
+        tank: "#3e51b1",
+        healer: "#3c672f",
+        dps: "#743333",
+        gatherer: "#a28838",
+        crafter: "#6748ad",
+      },
     },
-    extend: {},
   },
   daisyui: {
     themes: [
