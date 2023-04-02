3ab2ef3e1f7c104055456fe3fa1b749451448297
Ygor Goulart <ygorgm95@gmail.com>
2023-04-02 11:58:32 -0300

    fix(leaderboard): proper router handling

--- src/Pages/FreeCompany/Pages/Leaderboard/Leaderboard.tsx
+++ src/Pages/FreeCompany/Pages/Leaderboard/Leaderboard.tsx
@@ -2,24 +2,27 @@ import { FetchProgress } from "@/Components/LoadingComponents/FetchProgress";
 import { useFreeCompany } from "@/Contexts/FreeCompanyContext";
 import { StatsProvider } from "@/Contexts/StatsContext";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Mount } from "./Mount";
 import { Tabs } from "./Tabs";
 
 export const Leaderboard = () => {
-  const { MembersFullData, fetchProgress, loadStats } = useFreeCompany();
+  const { fetchProgress, loadStats } = useFreeCompany();
 
-  const location = useLocation();
-  const currentPath = location.pathname.split("/").reverse()[0];
-  const paths = ["Mount", "Minion", "Achievement"];
-  const isInDefault = !paths.includes(currentPath);
+  const isDefaultPath = /Leaderboard/.test(
+    useLocation()
+      .pathname.split("/")
+      .filter((e) => e !== "")
+      .reverse()[0]
+  );
 
   if (!loadStats) return <FetchProgress value={fetchProgress} />;
 
   return (
     <StatsProvider>
       <section className="grid min-h-[calc(100vh-448px)] gap-4 pb-8">
-        <Tabs />
+        <Tabs isDefaultPath={isDefaultPath} />
         <Outlet />
-        {isInDefault && <Navigate to="Mount" />}
+        {isDefaultPath ? <Mount /> : null}
       </section>
     </StatsProvider>
   );
--- src/Pages/FreeCompany/Pages/Leaderboard/Tabs.tsx
+++ src/Pages/FreeCompany/Pages/Leaderboard/Tabs.tsx
@@ -15,10 +15,21 @@ const Tab = ({ path, name }: { path: string; name: string }) => (
   </NavLink>
 );
 
-export const Tabs = () => {
+export const Tabs = ({ isDefaultPath }: { isDefaultPath: boolean }) => {
   return (
     <nav className="tabs tabs-boxed grid h-fit rounded-lg bg-base-100 p-2 md:grid-cols-3">
-      <Tab path="Mount" name="Mount" />
+      <NavLink
+        to="Mount"
+        key={uuidv4()}
+        className={({ isActive }) =>
+          isActive || isDefaultPath
+            ? "tab tab-lifted tab-active tab-lg duration-300"
+            : "tab tab-lifted tab-lg duration-300"
+        }
+      >
+        Mount
+      </NavLink>
+
       <Tab path="Minion" name="Minion" />
       <Tab path="Achievement" name="Achievement" />
     </nav>
