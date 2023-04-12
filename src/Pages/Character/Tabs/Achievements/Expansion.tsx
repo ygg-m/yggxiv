0ec2128758fc6df70198d5b758fd3b7d9249cdf7
Ygor Goulart <ygorgm95@gmail.com>
2023-04-12 17:20:52 -0300

    fix: background images

--- src/Pages/Character/Tabs/Achievements/Expansion.tsx
+++ src/Pages/Character/Tabs/Achievements/Expansion.tsx
@@ -17,35 +17,35 @@ const ExpansionList = ({ List }: { List: TreatedAchievementData[] }) => (
     <ExpansionCard
       Patch={2.0}
       Name="A Realm Reborn"
-      Background="https://img.finalfantasyxiv.com/lds/h/l/QpExCxHkd7zpxqXK1cl4Uz8bJ8.jpg?_ga=2.18938430.781238313.1680968566-1091794286.1670936645"
+      Background={BackgroundARR}
       LinkURL="ARealmReborn"
       List={List}
     />
     <ExpansionCard
       Patch={3.0}
       Name="Heavensward"
-      Background="https://fr.web.img6.acsta.net/pictures/19/06/27/18/09/2256112.jpg"
+      Background={BackgroundHW}
       LinkURL="Heavensward"
       List={List}
     />
     <ExpansionCard
       Patch={4.0}
       Name="Stormblood"
-      Background="https://m.media-amazon.com/images/S/aplus-media/vc/b4076b75-fa48-44a8-a3cc-2b2bca53e91b.jpg"
+      Background={BackgroundSB}
       LinkURL="Stormblood"
       List={List}
     />
     <ExpansionCard
       Patch={5.0}
       Name="Shadowbringers"
-      Background="https://www.spaziogames.it/wp-content/uploads/2019/02/Final-Fantasy-XIV_2019_02-02-19_023.jpg"
+      Background={BackgroundSHB}
       LinkURL="Shadowbringers"
       List={List}
     />
     <ExpansionCard
       Patch={6.0}
       Name="Endwalker"
-      Background="https://i.imgur.com/6Q3ZZHM.jpeg"
+      Background={BackgroundEW}
       LinkURL="Endwalker"
       List={List}
     />
@@ -83,13 +83,6 @@ const AchieveList = ({
   List.forEach((e) => {
     if (e.Obtained === true) Points += e.Data.Points;
   });
-
-  const isStrictDefaultPath = /Expansion|Mounts|Minions/.test(
-    useLocation()
-      .pathname.split("/")
-      .filter((e) => e !== "")
-      .reverse()[0]
-  );
 
   const Header = () => (
     <div className="relative z-0 flex items-center justify-between rounded-lg outline outline-1 outline-gray-600 before:absolute before:-z-10 before:h-full before:w-full before:rounded-lg before:bg-neutral before:bg-opacity-50">
