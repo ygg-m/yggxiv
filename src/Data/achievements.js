008fd3fb530b2d83853438f6facc4d0103f7cae7
Ygor Goulart <ygorgm95@gmail.com>
2023-04-15 18:16:38 -0300

    chores: add raids achievements to data files

--- src/Data/achievements.js
+++ src/Data/achievements.js
@@ -1,12 +1,219 @@
-export const QuestAchievements = [
-  {
-    ID: 1,
-    Patch: 2.0,
-  },
-];
+export const RaidAchievements = {
+  ARealmReborn: [
+    {
+      ID: 747,
+      Name: "The Binds That Tie I",
+    },
+    {
+      ID: 887,
+      Name: "In Another Bind I",
+    },
+    {
+      ID: 1040,
+      Name: "Out Of A Bind I",
+    },
+  ],
+  Heavensward: [
+    {
+      ID: 1228,
+      Name: "Sins Of The Father I",
+    },
+    {
+      ID: 1476,
+      Name: "Sins Of The Son I",
+    },
+    {
+      ID: 1639,
+      Name: "Sins Of The Creator I",
+    },
+  ],
+  Stormblood: [
+    {
+      ID: 1895,
+      Name: "I Am The Delta, I Am The Omega I",
+    },
+    {
+      ID: 2024,
+      Name: "I Am The Sigma, I Am The Omega I",
+    },
+    {
+      ID: 2118,
+      Name: "I Am The Alpha, I Am The Omega I",
+    },
+  ],
+  Shadowbringers: [
+    {
+      ID: 2409,
+      Name: "Paradise Found I",
+    },
+    {
+      ID: 2591,
+      Name: "Trouble In Paradise I",
+    },
+    {
+      ID: 2719,
+      Name: "Paradise Within Thee I",
+    },
+  ],
+  Endwalker: [
+    {
+      ID: 3035,
+      Name: "Could Be Worse I",
+    },
+    {
+      ID: 3108,
+      Name: "Gaze Of The Abyss I",
+    },
+  ],
+};
 
-export const RelicAchievements = [
-  {
-    ID: 1,
-  },
-];
+export const SavageAchievements = {
+  ARealmReborn: [
+    {
+      ID: 997,
+      Name: "A Flower By Any Other Name",
+    },
+    {
+      ID: 998,
+      Name: "Seconds",
+    },
+    {
+      ID: 999,
+      Name: "Obtanium",
+    },
+    {
+      ID: 1000,
+      Name: "Scent Of A Woman",
+    },
+  ],
+  Heavensward: [
+    {
+      ID: 1231,
+      Name: "Sins Of The Savage Father I",
+    },
+    {
+      ID: 1479,
+      Name: "Sins Of The Savage Son I",
+    },
+    {
+      ID: 1642,
+      Name: "Sins Of The Savage Creator I",
+    },
+  ],
+  Stormblood: [
+    {
+      ID: 1898,
+      Name: "I Am The Savage Delta, I Am The Savage Omega I",
+    },
+    {
+      ID: 2027,
+      Name: "I Am The Savage Sigma, I Am The Savage Omega I",
+    },
+    {
+      ID: 2121,
+      Name: "I Am The Savage Alpha, I Am The Savage Omega I",
+    },
+  ],
+  Shadowbringers: [
+    {
+      ID: 2412,
+      Name: "Savage Paradise Found I",
+    },
+    {
+      ID: 2594,
+      Name: "Savage Trouble In Paradise I",
+    },
+    {
+      ID: 2722,
+      Name: "Savage Paradise Within Thee I",
+    },
+  ],
+  Endwalker: [
+    {
+      ID: 3038,
+      Name: "Could Be Savage I",
+    },
+    {
+      ID: 3111,
+      Name: "Savage Gaze Of The Abyss I",
+    },
+  ],
+};
+
+export const UltimateAchievements = {
+  Stormblood: [
+    {
+      ID: 1993,
+      Name: "Resistance Is Futile",
+    },
+    {
+      ID: 2107,
+      Name: "Ultimatum",
+    },
+  ],
+  Shadowbringers: [
+    {
+      ID: 2444,
+      Name: "When I Ruled The World",
+    },
+    {
+      ID: 2107,
+      Name: "Ultimatum",
+    },
+  ],
+  Endwalker: [
+    {
+      ID: 3074,
+      Name: "As Suits A Hero",
+    },
+    {
+      ID: 3162,
+      Name: "Heart To Heartless",
+    },
+  ],
+};
+
+export const AllianceRaidAchievements = {
+  ARealmReborn: [
+    {
+      ID: 883,
+      Name: "You Call That A Labyrinth",
+    },
+    {
+      ID: 995,
+      Name: "Life Is A Syrcus",
+    },
+    {
+      ID: 1068,
+      Name: "Let The Sun Shine In",
+    },
+  ],
+  Heavensward: [
+    {
+      ID: 1399,
+      Name: "Touching The Void",
+    },
+    {
+      ID: 1574,
+      Name: "Ex Mhachina",
+    },
+    {
+      ID: 1689,
+      Name: "What's Dun Is Done",
+    },
+  ],
+  Stormblood: [
+    { ID: 1992, Name: "Zodiac Thriller" },
+    { ID: 2106, Name: "Didn't Stop, Made It Pop" },
+    { ID: 2164, Name: "Orbonne To Pick" },
+  ],
+  Shadowbringers: [
+    { ID: 2443, Name: "The First Law" },
+    { ID: 2622, Name: "Uncanny Valley" },
+    { ID: 2847, Name: "Deus Et Machina" },
+  ],
+  Endwalker: [
+    { ID: 3073, Name: "Divine Intervention" },
+    { ID: 3161, Name: "Divine Revelry" },
+  ],
+};
