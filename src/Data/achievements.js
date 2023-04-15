b50cd06065fa280ddec4f4622687d55db479ddbc
Ygor Goulart <ygorgm95@gmail.com>
2023-04-15 20:49:47 -0300

    chores: add achievementt data for expansion contents

--- src/Data/achievements.js
+++ src/Data/achievements.js
@@ -1,219 +1,266 @@
-export const RaidAchievements = {
-  ARealmReborn: [
-    {
-      ID: 747,
-      Name: "The Binds That Tie I",
-    },
-    {
-      ID: 887,
-      Name: "In Another Bind I",
-    },
-    {
-      ID: 1040,
-      Name: "Out Of A Bind I",
-    },
-  ],
-  Heavensward: [
-    {
-      ID: 1228,
-      Name: "Sins Of The Father I",
-    },
-    {
-      ID: 1476,
-      Name: "Sins Of The Son I",
-    },
-    {
-      ID: 1639,
-      Name: "Sins Of The Creator I",
-    },
-  ],
-  Stormblood: [
-    {
-      ID: 1895,
-      Name: "I Am The Delta, I Am The Omega I",
-    },
-    {
-      ID: 2024,
-      Name: "I Am The Sigma, I Am The Omega I",
-    },
-    {
-      ID: 2118,
-      Name: "I Am The Alpha, I Am The Omega I",
-    },
-  ],
-  Shadowbringers: [
-    {
-      ID: 2409,
-      Name: "Paradise Found I",
-    },
-    {
-      ID: 2591,
-      Name: "Trouble In Paradise I",
-    },
-    {
-      ID: 2719,
-      Name: "Paradise Within Thee I",
-    },
-  ],
-  Endwalker: [
-    {
-      ID: 3035,
-      Name: "Could Be Worse I",
-    },
-    {
-      ID: 3108,
-      Name: "Gaze Of The Abyss I",
-    },
-  ],
-};
+export const RaidAchievements = [
+  { ID: 747 },
+  { ID: 887 },
+  { ID: 1040 },
+  { ID: 1228 },
+  { ID: 1476 },
+  { ID: 1639 },
+  { ID: 1895 },
+  { ID: 2024 },
+  { ID: 2118 },
+  { ID: 2409 },
+  { ID: 2591 },
+  { ID: 2719 },
+  { ID: 3035 },
+  { ID: 3108 },
+];
 
-export const SavageAchievements = {
-  ARealmReborn: [
-    {
-      ID: 997,
-      Name: "A Flower By Any Other Name",
-    },
-    {
-      ID: 998,
-      Name: "Seconds",
-    },
-    {
-      ID: 999,
-      Name: "Obtanium",
-    },
-    {
-      ID: 1000,
-      Name: "Scent Of A Woman",
-    },
-  ],
-  Heavensward: [
-    {
-      ID: 1231,
-      Name: "Sins Of The Savage Father I",
-    },
-    {
-      ID: 1479,
-      Name: "Sins Of The Savage Son I",
-    },
-    {
-      ID: 1642,
-      Name: "Sins Of The Savage Creator I",
-    },
-  ],
-  Stormblood: [
-    {
-      ID: 1898,
-      Name: "I Am The Savage Delta, I Am The Savage Omega I",
-    },
-    {
-      ID: 2027,
-      Name: "I Am The Savage Sigma, I Am The Savage Omega I",
-    },
-    {
-      ID: 2121,
-      Name: "I Am The Savage Alpha, I Am The Savage Omega I",
-    },
-  ],
-  Shadowbringers: [
-    {
-      ID: 2412,
-      Name: "Savage Paradise Found I",
-    },
-    {
-      ID: 2594,
-      Name: "Savage Trouble In Paradise I",
-    },
-    {
-      ID: 2722,
-      Name: "Savage Paradise Within Thee I",
-    },
-  ],
-  Endwalker: [
-    {
-      ID: 3038,
-      Name: "Could Be Savage I",
-    },
-    {
-      ID: 3111,
-      Name: "Savage Gaze Of The Abyss I",
-    },
-  ],
-};
+export const SavageAchievements = [
+  { ID: 997 },
+  { ID: 998 },
+  { ID: 999 },
+  { ID: 1000 },
+  { ID: 1231 },
+  { ID: 1479 },
+  { ID: 1642 },
+  { ID: 1898 },
+  { ID: 2027 },
+  { ID: 2121 },
+  { ID: 2412 },
+  { ID: 2594 },
+  { ID: 2722 },
+  { ID: 3038 },
+  { ID: 3111 },
+];
 
-export const UltimateAchievements = {
-  Stormblood: [
-    {
-      ID: 1993,
-      Name: "Resistance Is Futile",
-    },
-    {
-      ID: 2107,
-      Name: "Ultimatum",
-    },
-  ],
-  Shadowbringers: [
-    {
-      ID: 2444,
-      Name: "When I Ruled The World",
-    },
-    {
-      ID: 2107,
-      Name: "Ultimatum",
-    },
-  ],
-  Endwalker: [
-    {
-      ID: 3074,
-      Name: "As Suits A Hero",
-    },
-    {
-      ID: 3162,
-      Name: "Heart To Heartless",
-    },
-  ],
-};
+export const UltimateAchievements = [
+  { ID: 1993 },
+  { ID: 2107 },
+  { ID: 2444 },
+  { ID: 2107 },
+  { ID: 3074 },
+  { ID: 3162 },
+];
 
-export const AllianceRaidAchievements = {
-  ARealmReborn: [
-    {
-      ID: 883,
-      Name: "You Call That A Labyrinth",
-    },
-    {
-      ID: 995,
-      Name: "Life Is A Syrcus",
-    },
-    {
-      ID: 1068,
-      Name: "Let The Sun Shine In",
-    },
-  ],
-  Heavensward: [
-    {
-      ID: 1399,
-      Name: "Touching The Void",
-    },
-    {
-      ID: 1574,
-      Name: "Ex Mhachina",
-    },
-    {
-      ID: 1689,
-      Name: "What's Dun Is Done",
-    },
-  ],
-  Stormblood: [
-    { ID: 1992, Name: "Zodiac Thriller" },
-    { ID: 2106, Name: "Didn't Stop, Made It Pop" },
-    { ID: 2164, Name: "Orbonne To Pick" },
-  ],
-  Shadowbringers: [
-    { ID: 2443, Name: "The First Law" },
-    { ID: 2622, Name: "Uncanny Valley" },
-    { ID: 2847, Name: "Deus Et Machina" },
-  ],
-  Endwalker: [
-    { ID: 3073, Name: "Divine Intervention" },
-    { ID: 3161, Name: "Divine Revelry" },
-  ],
-};
+export const AllianceRaidAchievements = [
+  { ID: 883 },
+  { ID: 995 },
+  { ID: 1068 },
+  { ID: 1399 },
+  { ID: 1574 },
+  { ID: 1689 },
+  { ID: 1992 },
+  { ID: 2106 },
+  { ID: 2164 },
+  { ID: 2443 },
+  { ID: 2622 },
+  { ID: 2847 },
+  { ID: 3073 },
+  { ID: 3161 },
+];
+
+export const ExtremeAchievements = [
+  { ID: 855 },
+  { ID: 856 },
+  { ID: 857 },
+  { ID: 893 },
+  { ID: 894 },
+  { ID: 994 },
+  { ID: 1220 },
+  { ID: 1221 },
+  { ID: 1485 },
+  { ID: 1636 },
+  { ID: 1685 },
+  { ID: 1901 },
+  { ID: 1902 },
+  { ID: 2023 },
+  { ID: 2117 },
+  { ID: 2165 },
+  { ID: 2385 },
+  { ID: 2386 },
+  { ID: 2590 },
+  { ID: 2621 },
+  { ID: 2718 },
+  { ID: 3107 },
+  { ID: 3160 },
+  { ID: 2109 },
+  { ID: 2586 },
+];
+
+export const PalaceOfTheDeadAchievements = [
+  { ID: 1576 },
+  { ID: 1577 },
+  { ID: 1578 },
+  { ID: 1579 },
+  { ID: 1580 },
+  { ID: 1655 },
+  { ID: 1656 },
+  { ID: 1657 },
+];
+
+export const PalaceOfTheDeadSoloAchievements = [
+  { ID: 1659 },
+  { ID: 1660 },
+  { ID: 1952 },
+];
+
+export const HeavenOnHighAchievements = [
+  { ID: 2047 },
+  { ID: 2048 },
+  { ID: 2049 },
+  { ID: 2050 },
+  { ID: 2051 },
+];
+
+export const HeavenOnHighSoloAchievements = [{ ID: 2054 }, { ID: 2055 }];
+
+export const BlueMage = [
+  { ID: 2203 },
+  { ID: 2203 },
+  { ID: 2203 },
+  { ID: 2203 },
+  { ID: 2449 },
+];
+
+export const BlueMageFeats = [
+  { ID: 2209 },
+  { ID: 2210 },
+  { ID: 2211 },
+  { ID: 2450 },
+  { ID: 2727 },
+];
+
+export const VariantDungeonAchievements = [
+  { ID: 3148 },
+  { ID: 3149 },
+  { ID: 3150 },
+  { ID: 3151 },
+  { ID: 3155 },
+];
+
+export const VariantDungeonSavageAchievements = [{ ID: 3156 }];
+
+export const EurekaOrthosAchievements = [
+  { ID: 3174 },
+  { ID: 3175 },
+  { ID: 3176 },
+  { ID: 3177 },
+  { ID: 3178 },
+];
+export const EurekaOrthosSoloAchievements = [{ ID: 3182 }, { ID: 3183 }];
+
+export const EurekaAchievements = [
+  { ID: 2045 },
+  { ID: 2097 },
+  { ID: 2159 },
+  { ID: 2231 },
+  { ID: 2242 },
+];
+
+export const BalesionArsenal = { ID: 2227 };
+
+export const ZodiacWeaponAchievements = [{ ID: 1054 }, { ID: 1081 }];
+
+export const AnimaWeaponAchievements = [
+  { ID: 1708 },
+  { ID: 1709 },
+  { ID: 1710 },
+  { ID: 1711 },
+  { ID: 1712 },
+  { ID: 1713 },
+  { ID: 1714 },
+  { ID: 1715 },
+  { ID: 1716 },
+  { ID: 1717 },
+  { ID: 1718 },
+  { ID: 1719 },
+  { ID: 1720 },
+];
+
+export const EurekaWeaponAchievements = [
+  { ID: 2212 },
+  { ID: 2213 },
+  { ID: 2214 },
+  { ID: 2215 },
+  { ID: 2216 },
+  { ID: 2217 },
+  { ID: 2218 },
+  { ID: 2219 },
+  { ID: 2220 },
+  { ID: 2221 },
+  { ID: 2222 },
+  { ID: 2223 },
+  { ID: 2224 },
+  { ID: 2225 },
+  { ID: 2226 },
+];
+
+export const ResistanceWeaponAchievements = [
+  { ID: 2857 },
+  { ID: 2858 },
+  { ID: 2859 },
+  { ID: 2860 },
+  { ID: 2861 },
+  { ID: 2862 },
+  { ID: 2863 },
+  { ID: 2864 },
+  { ID: 2865 },
+  { ID: 2866 },
+  { ID: 2867 },
+  { ID: 2868 },
+  { ID: 2869 },
+  { ID: 2870 },
+  { ID: 2871 },
+  { ID: 2872 },
+  { ID: 2873 },
+];
+
+export const MandervilleWeaponAchievements = [
+  { ID: 3224 },
+  { ID: 3225 },
+  { ID: 3226 },
+  { ID: 3227 },
+  { ID: 3228 },
+  { ID: 3229 },
+  { ID: 3230 },
+  { ID: 3231 },
+  { ID: 3232 },
+  { ID: 3233 },
+  { ID: 3234 },
+  { ID: 3235 },
+  { ID: 3236 },
+  { ID: 3237 },
+  { ID: 3238 },
+  { ID: 3239 },
+  { ID: 3240 },
+  { ID: 3241 },
+  { ID: 3242 },
+];
+
+export const SkySteelToolsAchievements = [
+  { ID: 2787 },
+  { ID: 2788 },
+  { ID: 2789 },
+  { ID: 2790 },
+  { ID: 2791 },
+  { ID: 2792 },
+  { ID: 2793 },
+  { ID: 2794 },
+  { ID: 2795 },
+  { ID: 2796 },
+  { ID: 2797 },
+];
+
+export const SplendorousToolsAchievements = [
+  { ID: 3193 },
+  { ID: 3194 },
+  { ID: 3195 },
+  { ID: 3196 },
+  { ID: 3197 },
+  { ID: 3198 },
+  { ID: 3199 },
+  { ID: 3200 },
+  { ID: 3201 },
+  { ID: 3202 },
+  { ID: 3203 },
+];
